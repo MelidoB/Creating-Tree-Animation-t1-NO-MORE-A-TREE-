ff955e25b3c9d9a44e7b856ad98e864aa6d89a79
MelidoB <48568341+MelidoB@users.noreply.github.com>
2020-08-17 14:10:09 -0400

    Add files via upload

---
+++ js/book-tree-animation.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $(".animation--book-1,#animation--darkcover-closer").click(function(){
+    $("#animation--darkcover,#animation--darkcover-closer").toggle();
+  });
+});
